29be99ad5e5727a15085f5ffd4438a8d6b9ed947
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2023-03-02 13:39:03 +0700

    uninstall angular

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const router = require('./myRouter')
 
+const Movie = require('./movie')
+
 
 app.use(router)
 
 app.listen(3000,() => {
     console.log('start server')
+    // Movie.find().exec((err,doc) => {
+    //     console.log(doc)
+    // })
+
 })
--- Backend/myRouter.js
+++ Backend/myRouter.js
@@ -1,44 +1,66 @@
+const { exec } = require('child_process')
 const express = require('express')
 const router = express.Router()
 const path = require('path')
 //เรียกใช้งานโมเดล
 const Movie = require('./movie')
 
-// router.get("/",(req,res)=>{
-//     res.write("hello g")
-//     res.end()
-// })
+router.get("/",(req,res)=>{
+    res.write("hello g")
+    res.end()
+})
 
 // router.get("/:category/:id",(req,res)=>{
 //     res.write("hello g")
 //     res.end()
 // })
-router.get("/romantic/:id",(req,res)=>{
-    res.write("hello g")
-    res.end()
+router.get("/romantic/:name",(req,res)=>{
+    const movie_name = req.params.name
+    console.log(movie_name)
+    Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
-router.get("/comedy/:id",(req,res)=>{
-    res.write("hello d")
-    res.end()
+router.get("/comedy/:name",(req,res)=>{
+    const movie_name = req.params.name
+    console.log(movie_name)
+    Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
-router.get("/fantasy/:id",(req,res)=>{
-    res.write("hello a")
-    res.end()
+router.get("/fantasy/::name",(req,res)=>{
+    const movie_name = req.params.name
+    console.log(movie_name)
+    Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
-router.get("/kids/:id",(req,res)=>{
-    
+router.get("/kids/:name",(req,res)=>{
+    const movie_name = req.params.name
+    console.log(movie_name)
+    Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
-router.get("/drama/:id",(req,res)=>{
-    
+router.get("/drama/:name",(req,res)=>{
+    const movie_name = req.params.name
+    console.log(movie_name)
+    Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
-router.get("/action/:id",(req,res)=>{
-    
+router.get("/action/:name",(req,res)=>{
+    const movie_name = req.params.name
+    console.log(movie_name)
+    Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
     })
-   
+    })
+
 
 module.exports = router

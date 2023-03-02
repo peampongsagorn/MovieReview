5ba879b73abcdad04e13fc9f1c3c03ec5df8d1bf
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2023-03-03 00:36:10 +0700

    comment model

--- Backend/myRouter.js
+++ Backend/myRouter.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const path = require('path')
 //เรียกใช้งานโมเดล
 const Movie = require('./movie')
-
+const Comment = require('./comment')
 router.get("/",(req,res)=>{
     res.write("hello g")
     res.end()
@@ -20,12 +20,18 @@ router.get("/romantic/:name",(req,res)=>{
     Movie.findOne({name:movie_name}).exec((err,doc) => {
         console.log(doc)
     })
+    Comment.find({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
 router.get("/comedy/:name",(req,res)=>{
     const movie_name = req.params.name
     console.log(movie_name)
     Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
+    Comment.findOne({name:movie_name}).exec((err,doc) => {
         console.log(doc)
     })
     })
@@ -36,12 +42,18 @@ router.get("/fantasy/::name",(req,res)=>{
     Movie.findOne({name:movie_name}).exec((err,doc) => {
         console.log(doc)
     })
+    Comment.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
 router.get("/kids/:name",(req,res)=>{
     const movie_name = req.params.name
     console.log(movie_name)
     Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
+    Comment.findOne({name:movie_name}).exec((err,doc) => {
         console.log(doc)
     })
     })
@@ -52,16 +64,85 @@ router.get("/drama/:name",(req,res)=>{
     Movie.findOne({name:movie_name}).exec((err,doc) => {
         console.log(doc)
     })
+    Comment.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)
+    })
     })
 
 router.get("/action/:name",(req,res)=>{
     const movie_name = req.params.name
     console.log(movie_name)
     Movie.findOne({name:movie_name}).exec((err,doc) => {
+        console.log(doc)    
+    })
+    Comment.findOne({name:movie_name}).exec((err,doc) => {
         console.log(doc)
-        
     })
     })
 
+router.post('/romantic/:name',(req,res) => {
+    let data = new Comment({
+        name :req.params.name,
+        ment :req.body.comment
+    })
+    Comment.saveComment(data,(err) => {
+        if(err) console.log(err)
+        res.redirect("/romantic/:name")
+    })
+})
 
+router.post('/comedy/:name',(req,res) => {
+    let data = new Comment({
+        name :req.params.name,
+        ment :req.body.comment
+    })
+    Comment.saveComment(data,(err) => {
+        if(err) console.log(err)
+        res.redirect("/romantic/:name")
+    })
+})
+
+router.post('/fantasy/:name',(req,res) => {
+    let data = new Comment({
+        name :req.params.name,
+        ment :req.body.comment
+    })
+    Comment.saveComment(data,(err) => {
+        if(err) console.log(err)
+        res.redirect("/romantic/:name")
+    })
+})
+
+router.post('/kids/:name',(req,res) => {
+    let data = new Comment({
+        name :req.params.name,
+        ment :req.body.comment
+    })
+    Comment.saveComment(data,(err) => {
+        if(err) console.log(err)
+        res.redirect("/romantic/:name")
+    })
+})
+
+router.post('/drama/:name',(req,res) => {
+    let data = new Comment({
+        name :req.params.name,
+        ment :req.body.comment
+    })
+    Comment.saveComment(data,(err) => {
+        if(err) console.log(err)
+        res.redirect("/romantic/:name")
+    })
+})
+
+router.post('/action/:name',(req,res) => {
+    let data = new Comment({
+        name :req.params.name,
+        ment :req.body.comment
+    })
+    Comment.saveComment(data,(err) => {
+        if(err) console.log(err)
+        res.redirect("/romantic/:name")
+    })
+})
 module.exports = router

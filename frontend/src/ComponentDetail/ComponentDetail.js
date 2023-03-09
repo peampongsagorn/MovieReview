ec56a425e19bacb28d9eeeba29b68a3cdace18e1
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2023-03-10 01:36:13 +0700

    back

--- frontend/src/ComponentDetail/ComponentDetail.js
+++ frontend/src/ComponentDetail/ComponentDetail.js
@@ -1,71 +1,36 @@
 import React, { useEffect, useState } from "react"
 import axios from 'axios'
-//import { movie } from "../../../Backend/myrouter";
+
 
 function ComponentDetail(){
-    //axios.get('http://localhost:8080/Romantic/titanic', { crossdomain: true})
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [imagepath, setImagapath] = useState("");
-    const [comment, setComment] = useState("");
+    const [name, setName] = useState([]);
+    
 
-    function getMovieDetail(){
-        axios.get('http://localhost:8080/Romantic/titanic', { crossdomain: true})
-        .then(response => {
-            setName(response.data.name);
-            setDescription(response.data.description);
-            setImagapath(response.data.imagepath);
-            //setComment(response.data.comment);
-        })
-    }
+    // function getMovieDetail(){
+    //     axios.get('http://localhost:8080/Romantic/titanic', { crossdomain: true})
+    //     .then(response => {
+    //         setName(response.data.name);
+    //         setDescription(response.data.description);
+    //         setImagapath(response.data.imagepath);
+    //     })
+    // }
+    useEffect(() =>  {
+        const fetchData = async() => {
+            const result = await fetch('http://localhost:8080/Romantic/titanic')
+            const jsonResult = await result.json()
 
+            setName(jsonResult)
+        }
 
-<<<<<<< HEAD
-// const movies = axios.create({
-//     baseURL: "http://localhost:8080/Romantic/titanic"
-// })
-=======
-const ComponentDetail = () => {
-    const [movies, setMovie] = useState([]);
-    // const [description, setDescription] = useState("");
-    // const [imagepath, setImagapath] = useState("");
-    // const [comment, setComment] = useState("");
->>>>>>> ac406f724f538b2b259abf65baabb82709309e12
-
-// const ComponentDetail = () => {
-//     const [movies, setMovie] = useState([]);
-//     // const [description, setDescription] = useState("");
-//     // const [imagepath, setImagapath] = useState("");
-//     // const [comment, setComment] = useState("");
-
-//     useEffect(() => {
-//         movies.get('?_limit=10', { crossdomain: true})
-//                 .then((response) => {
-//                     setMovie(response.data);
-//                     // setDescription(response.data.description);
-//                     // setImagapath(response.data.imagepath);
-//                     // setComment(response.data.comment);
-//     })
-//             },[])
+        fetchData()
+    }, [])
 
 return(
     
-<<<<<<< HEAD
            // <div key={movies.id}>
             <h2> {name}</h2>
            // </div>
-=======
-            <div key={movies.id}>
-            <h2> {movies.name}</h2>
-            
-            </div>
->>>>>>> ac406f724f538b2b259abf65baabb82709309e12
         )
 }
 
-<<<<<<< HEAD
-=======
-} 
-
->>>>>>> ac406f724f538b2b259abf65baabb82709309e12
 export default ComponentDetail
